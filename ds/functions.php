5afe5587b34997099218839bade3b888a01b4133
nilb7 <tieenn298@gmail.com>
2025-09-02 20:42:20 +0200

    perseritje

---
+++ ds/functions.php
@@ -0,0 +1,11 @@
+<?php
+function ds_enqueue_scripts() {
+    wp_enqueue_style( 'main-style', get_stylesheet_uri( ) );
+
+
+}
+
+add_action('ds_enqueue_scripts'.)
+
+
+?>
---
+++ ds/index.php
@@ -0,0 +1,5 @@
+<?php get_header();?>
+
+<h1> This is out Ds theme</h1>
+
+<?php get_footer();?>
